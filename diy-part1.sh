e0e9ecc816b8fdf998e02144a389057acb0bfcd2
wjc0325 <128901336+wjc0325@users.noreply.github.com>
2023-03-26 21:15:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
-git clone https://github.com/kenzok8/small.git  package/openwrt-small
-git clone https://github.com/jerrykuku/luci-theme-argon.git
