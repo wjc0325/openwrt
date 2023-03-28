820f4a30bbb19d5a8870206efe5fad14236230af
wjc0325 <128901336+wjc0325@users.noreply.github.com>
2023-03-28 23:21:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+git clone https://github.com/jerrykuku/luci-theme-argon.git
