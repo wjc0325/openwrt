f309c67ff19d6003f9371b2975a92ae9de7a8d13
wjc0325 <128901336+wjc0325@users.noreply.github.com>
2023-03-29 22:56:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,5 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git
